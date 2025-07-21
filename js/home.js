4592b79bec2166f5afb58e6d8c8636e75e5f0fa9
Duc Hoi Chau <dcha0120@student.monash.edu>
2025-07-21 19:53:16 +1000

    capstone pet

---
+++ js/home.js
@@ -0,0 +1,37 @@
+function home_hiddenButton() {
+  document.getElementById("homeButton").style.display = "none";
+  document.querySelector(".video video").play();
+}
+
+function initSlick() {
+  var $slider = $(".row_testimonials");
+
+  if (window.matchMedia("(max-width: 576px)").matches) {
+    $slider.slick({
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      infinite: true,
+      dots: true,
+      arrows: false,
+      autoplay: true,
+    });
+  } else {
+    $slider.slick({
+      slidesToShow: 2, // Adjust as needed for larger screens
+      slidesToScroll: 1,
+      infinite: true,
+      dots: true,
+      arrows: false,
+      autoplay: true,
+    });
+  }
+}
+
+// Initialize on page load
+initSlick();
+
+// Re-initialize on window resize
+$(window).resize(function () {
+  $(".row_testimonials").slick("unslick"); // Destroy existing slider
+  initSlick(); // Reinitialize with new settings
+});
